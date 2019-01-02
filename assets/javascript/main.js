2554d01a48b02c8af92a2ce20f7f1366988001e3
Neil Dhand <neildhand@Neils-MacBook-Pro.local>
2019-01-01 23:35:42 -0600

    now displays rating, reviews, actors, directors, and displays default image if no image found

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,8 @@
 var form = document.getElementById("movie-form");
+
 form.onsubmit = function(event) {
     event.preventDefault();
+
     console.log(form.movieTitle.value);
 
     var title = form.movieTitle.value;
@@ -15,47 +17,170 @@ form.onsubmit = function(event) {
             console.log(`success: ${result.Response}`);
             console.log(result);
 
-            for(var i = 0; i < result.Search.length; i++) {
-                var movieId = result.Search[i].imdbID;
+            if(!document.getElementById('result').hasChildNodes()) {
+                console.log('has no children');
+
                 var divEl = document.createElement('div');
                 var descDiv = document.createElement('div');
-
-                $.ajax({
-                    url: `http://www.omdbapi.com/?i=${movieId}&plot=full&apikey=7c05aa35`,
-                    method: "GET",
-                }).done(function(res) {
-
-                    var imgEl = document.createElement('img');
-                    imgEl.src = res.Poster;
     
-                    descDiv.appendChild(imgEl);
+                for(var i = 0; i < result.Search.length; i++) {
+                    var movieId = result.Search[i].imdbID;
     
-                    var titleH2 = document.createElement('h2');
-                    var title = res.Title;
-                    var year = res.Year;
-                    var titleText = document.createTextNode(`${title} (${year})`);
-                    titleH2.appendChild(titleText);
-                    descDiv.appendChild(titleH2);
-
-                    var actors;
-
-                    console.log('Res: ', res);
-                    var plotP = document.createElement('p');
-                    var plot = res.Plot;
-                    var plotText = document.createTextNode(plot);
-                    plotP.appendChild(plotText);
-
-                    descDiv.appendChild(plotP);
-
-                });
-                
-                divEl.appendChild(descDiv);
-                
+                    $.ajax({
+                        url: `http://www.omdbapi.com/?i=${movieId}&plot=full&apikey=7c05aa35`,
+                        method: "GET",
+                    }).done(function(res) {
     
-                resultDiv = document.getElementById('result');
-                resultDiv.appendChild(divEl);
+                        var imgEl = document.createElement('img');
+
+                        console.log(res.Poster);
+
+                        if(res.Poster === "N/A") {
+                            imgEl.src = './assets/images/no-image-found-360x260.png';
+                        } else {
+                            imgEl.src = res.Poster;
+                        }
+        
+                        descDiv.appendChild(imgEl);
+        
+                        var titleH2 = document.createElement('h2');
+                        var title = res.Title;
+                        var year = res.Year;
+                        var titleText = document.createTextNode(`${title} (${year})`);
+                        titleH2.appendChild(titleText);
+                        descDiv.appendChild(titleH2);
+    
+                        var directorH5 = document.createElement('h5');
+                        directorText = document.createTextNode(`Directed by: ${res.Director}`);
+                        directorH5.style.fontWeight = 'bold';
+                        directorH5.appendChild(directorText);
+                        descDiv.appendChild(directorH5);
+    
+                        var actors = res.Actors;
+                        var actorsH5 = document.createElement('h5');
+                        var actorsText = document.createTextNode(`Actors: ${actors}`);
+                        actorsH5.style.fontWeight = 'bold';
+                        actorsH5.appendChild(actorsText);
+                        descDiv.appendChild(actorsH5);
+    
+                        var moreInfoH6 = document.createElement('h6');
+                        moreInfoText = document.createTextNode(`Genre: ${res.Genre}. Rated: ${res.Rated}. Runtime: ${res.Runtime}`);
+                        moreInfoH6.appendChild(moreInfoText);
+                        descDiv.appendChild(moreInfoH6);
+    
+                        var ratingsDiv = document.createElement('div');
+                        for(var i = 0; i < res.Ratings.length; i++) {
+                            console.log(`Ratings: ${res.Ratings[i].Source}: ${res.Ratings[i].Value}`);
+                            var ratingsH6 = document.createElement('h6');
+                            var ratingsText = document.createTextNode(`${res.Ratings[i].Source}: ${res.Ratings[i].Value}`);
+                            ratingsH6.appendChild(ratingsText);
+                            ratingsDiv.appendChild(ratingsH6);
+                        }
+                        descDiv.appendChild(ratingsDiv);
+    
+                        var ratingsH6 = document.createElement('h6');
+                        //var ratingsText = document.createTextNode()
+    
+                        console.log('Res: ', res);
+                        var plotP = document.createElement('p');
+                        var plot = res.Plot;
+                        var plotText = document.createTextNode(plot);
+                        plotP.appendChild(plotText);
+    
+                        descDiv.appendChild(plotP);
+    
+                    });
+                    
+                    divEl.appendChild(descDiv);
+                    
+        
+                    resultDiv = document.getElementById('result');
+                    resultDiv.appendChild(divEl);
+                }
+            } else {
+                console.log('has children');
+                document.getElementById('result').innerHTML = "";
+
+                var divEl = document.createElement('div');
+                var descDiv = document.createElement('div');
+    
+                for(var i = 0; i < result.Search.length; i++) {
+                    var movieId = result.Search[i].imdbID;
+    
+                    $.ajax({
+                        url: `http://www.omdbapi.com/?i=${movieId}&plot=full&apikey=7c05aa35`,
+                        method: "GET",
+                    }).done(function(res) {
+    
+                        var imgEl = document.createElement('img');
+
+                        if(res.Poster == "N/A") {
+                            imgEl.src = './images/no-image-found-360x260.png';
+                        } else {
+                            imgEl.src = res.Poster;
+                        }
+                        
+        
+                        descDiv.appendChild(imgEl);
+        
+                        var titleH2 = document.createElement('h2');
+                        var title = res.Title;
+                        var year = res.Year;
+                        var titleText = document.createTextNode(`${title} (${year})`);
+                        titleH2.appendChild(titleText);
+                        descDiv.appendChild(titleH2);
+    
+                        var directorH5 = document.createElement('h5');
+                        directorText = document.createTextNode(`Directed by: ${res.Director}`);
+                        directorH5.style.fontWeight = 'bold';
+                        directorH5.appendChild(directorText);
+                        descDiv.appendChild(directorH5);
+    
+                        var actors = res.Actors;
+                        var actorsH5 = document.createElement('h5');
+                        var actorsText = document.createTextNode(`Actors: ${actors}`);
+                        actorsH5.style.fontWeight = 'bold';
+                        actorsH5.appendChild(actorsText);
+                        descDiv.appendChild(actorsH5);
+    
+                        var moreInfoH6 = document.createElement('h6');
+                        moreInfoText = document.createTextNode(`Genre: ${res.Genre}. Rated: ${res.Rated}. Runtime: ${res.Runtime}`);
+                        moreInfoH6.appendChild(moreInfoText);
+                        descDiv.appendChild(moreInfoH6);
+    
+                        var ratingsDiv = document.createElement('div');
+                        for(var i = 0; i < res.Ratings.length; i++) {
+                            console.log(`Ratings: ${res.Ratings[i].Source}: ${res.Ratings[i].Value}`);
+                            var ratingsH6 = document.createElement('h6');
+                            var ratingsText = document.createTextNode(`${res.Ratings[i].Source}: ${res.Ratings[i].Value}`);
+                            ratingsH6.appendChild(ratingsText);
+                            ratingsDiv.appendChild(ratingsH6);
+                        }
+                        descDiv.appendChild(ratingsDiv);
+    
+                        var ratingsH6 = document.createElement('h6');
+                        //var ratingsText = document.createTextNode()
+    
+                        console.log('Res: ', res);
+                        var plotP = document.createElement('p');
+                        var plot = res.Plot;
+                        var plotText = document.createTextNode(plot);
+                        plotP.appendChild(plotText);
+    
+                        descDiv.appendChild(plotP);
+    
+                    });
+                    
+                    divEl.appendChild(descDiv);
+                    
+        
+                    resultDiv = document.getElementById('result');
+                    resultDiv.appendChild(divEl);
+                }
             }
 
         });
+
+        return;
 
 };
